9c1737b80fa5cc6486a3f267e742bd2e4b6ce678
Konstantin Fedchenko <kostya.fedchenko.00@mail.ru>
2020-03-03 17:49:42 +0600

    last commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
 
+const Album = require('./app/Album');
+const Artist = require('./app/Artist');
+const Track = require('./app/Track');
+
+const config = require('./config');
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+const run = async () => {
+    await mongoose.connect('mongodb://localhost', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+    app.use('/artist', Artist);
+    app.use('/album', Album);
+    app.use('/track', Track);
+
+    app.listen(config.port, () => {
+        console.log(`Server started on ${config.port} port!`)
+    })
+};
+
+run().catch(e => {
+    console.error(e)
+});
